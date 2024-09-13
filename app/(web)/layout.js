c673552c1e74c7e98de02d9b78650c7a9dc4b690
Elango Palanisamy <elango@Elangos-Mac-mini.local>
2024-09-13 18:36:04 +0530

    added new metadata

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "AltDaily News",
-  description: "One of the leading newsletter",
+  description: "Alternative insights, fresh off the press",
 };
 
 export default function RootLayout({ children }) {
