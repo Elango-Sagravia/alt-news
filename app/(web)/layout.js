b886c864649d731f59c6a79aa0820caffc3427e3
Elango-Sagravia <elango@sagravia.com>
2025-04-10 14:28:58 +0530

    fixed name in gsc

--- app/(web)/layout.js
+++ app/(web)/layout.js
@@ -9,6 +9,7 @@ import Footer from "@/components/ui/footer/footer";
 import AppProvider from "@/context/appContext";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -79,7 +80,7 @@ const nameJsonLd = {
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning={true} lang="en" className={inter.className}>
-      <head>
+      <Head>
         <Script
           id="schema"
           type="application/ld+json"
@@ -87,11 +88,18 @@ export default function RootLayout({ children }) {
             __html: JSON.stringify(jsonLd),
           }}
         />
-        <Script
+        <script
           id="schema-name"
           type="application/ld+json"
-          strategy="beforeInteractive" // Make sure it's loaded early
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(nameJsonLd) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "Long and Short",
+              alternateName: "Long & Short",
+              url: "https://www.longandshort.com",
+            }),
+          }}
         />
         <meta
           name="viewport"
@@ -130,7 +138,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta name="application-name" content="Long and Short" />
         <meta property="og:site_name" content="Long and Short" />
         <meta name="apple-mobile-web-app-title" content="Long and Short" />
-      </head>
+      </Head>
 
       <body>
         <ThemeProvider
